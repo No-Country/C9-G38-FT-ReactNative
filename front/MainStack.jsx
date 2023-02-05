9189dd7d6baa2ec3bde79328162db06882e930f6
camelCase <caracido@gmail.com>
2023-02-05 19:32:57 -0300

    feat: upload routes with  examples

--- front/MainStack.jsx
+++ front/MainStack.jsx
@@ -1,14 +1,16 @@
 import { createStackNavigator } from '@react-navigation/stack';
-import Home from './src/views/Home.jsx';
+
+import Home from './src/views/Home';
 import Login from './src/views/Login';
 import Profile from './src/views/Profile';
-import Register from './src/views/Register.js';
+import Register from './src/views/Register';
 
 const Stack = createStackNavigator();
 
 export function Navigation() {
   return (
-    <Stack.Navigator initialRouteName='Login'>
+    <Stack.Navigator initialRouteName='Home'>
+      
       <Stack.Screen options={{headerShown: false}} name="Login" component={Login} />
       <Stack.Screen name="Register" component={Register} />
       <Stack.Screen name="Home" component={Home} />
--- front/src/views/Profile.js
+++ front/src/views/Profile.js
@@ -1,13 +1,41 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
-function Profile() {
-    return (
-      <View>
-        <Text>Hi desde Profile</Text>
-      </View>
-    );
-  }
-  
-  export default Profile;
-  
+
+const Profile = ({navigation, route}) => {
+
+  const {someText} = route.params
+
+  return (
+    <View>
+      <Text>{JSON.stringify(someText)}</Text>
+     
+      <TouchableOpacity onPress={
+        ()=> navigation.goBack()}>
+        <Text>VOLVER</Text>
+      </TouchableOpacity>
+
+
+      <TouchableOpacity onPress={
+        ()=> navigation.navigate("Profile",{...route.params})}>
+        <Text>PROFILE(con .navigate)</Text>
+      </TouchableOpacity>
+
+
+
+
+
+      <TouchableOpacity onPress={()=> navigation.push("Profile",{...route.params})}>
+        <Text>PROFILE(con .push)</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={()=> navigation.navigate("Home")}>
+        <Text>IR HOME</Text>
+      </TouchableOpacity>
+    
+
+    </View>
+  );
+};
+
+export default Profile;
