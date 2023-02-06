ce6a6bc21d0b3b366172d898d0ed4e67ae62d682
Ren-9 <braianmartinez648@gmail.com>
2023-02-06 18:27:38 -0400

    fix: export default

--- front/src/views/Register.js
+++ front/src/views/Register.js
@@ -1,10 +1,12 @@
-import { View } from 'react-native'
-import { Form } from '../features/register/components'
+import { View } from 'react-native';
+import { Form } from '../features/register/components';
 
-export function Register() {
-    return (
-        <View>
-            <Form />
-        </View>
-    )
+function Register() {
+  return (
+    <View>
+      <Form />
+    </View>
+  );
 }
+
+export default Register;
