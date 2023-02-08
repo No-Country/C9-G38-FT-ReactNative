5705af010e854427c0f968b00af18d7eaf91d9c8
Luem <luchemma@gmail.com>
2023-02-08 12:03:48 -0300

    guide to do home view (preview)

--- front/src/features/home/components/HomeHeader.js
+++ front/src/features/home/components/HomeHeader.js
@@ -2,8 +2,30 @@ import { Text, View } from 'react-native'
 
 const HomeHeader = () => {
     return (
-        <View>
-            <Text>Init Home Header</Text>
+        <View
+            style={{
+                flex: 0,
+                flexDirection: 'row',
+                margin: 5,
+                border: 1,
+                borderRadius: 4,
+                padding: 15,
+                backgroundColor: '#5192',
+            }}
+        >
+            <View>
+                <Text
+                    style={{
+                        color: 'black',
+                        fontWeight: '700',
+                    }}
+                >
+                    LOGO APP
+                </Text>
+            </View>
+            <View>
+                <Text>Notifications ICON</Text>
+            </View>
         </View>
     )
 }
