a376a9e6769f07634635d98ff3f289ef3000551f
Luem <luchemma@gmail.com>
2023-02-08 10:59:21 -0300

    init calendar, search and feed views

---
+++ front/src/views/Calendar.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+
+const Calendar = () => {
+    return (
+        <View>
+            <Text>Calendar</Text>
+            <Text>Proximos Eventos:</Text>
+        </View>
+    )
+}
+
+export default Calendar
---
+++ front/src/views/Feed.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+
+const Feed = () => {
+    return (
+        <View>
+            <Text>Feed:</Text>
+            <Text>User 1:</Text>
+            <Text>Events:</Text>
+        </View>
+    )
+}
+
+export default Feed
