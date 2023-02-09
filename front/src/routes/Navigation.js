d8ae0612f55f04224976a0f9756886ee7abe23b4
Luem <luchemma@gmail.com>
2023-02-09 10:32:30 -0300

    first update

--- front/src/routes/Navigation.js
+++ front/src/routes/Navigation.js
@@ -1,52 +1,52 @@
-import { useEffect, Fragment } from 'react';
-import { useAuthStore } from '../store/authStore';
+import { useEffect, Fragment } from 'react'
+import { useAuthStore } from '../store/authStore'
 
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack'
 
-import Home from '../views/Home';
-import Login from '../views/Login';
-import Profile from '../views/Profile';
-import Register from '../views/Register';
-import Onboarding from '../views/Onboarding';
-import Map from '../features/profile/components/Maps.jsx';
+import Home from '../views/Home'
+import Login from '../views/Login'
+import Profile from '../views/Profile'
+import Register from '../views/Register'
+import Onboarding from '../views/Onboarding'
+import Map from '../features/profile/components/Maps.jsx'
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
 export function Navigation() {
-  const authToken = useAuthStore((state) => state.authToken);
-  const getAuth = useAuthStore((state) => state.getAuth);
+    const authToken = useAuthStore(state => state.authToken)
+    const getAuth = useAuthStore(state => state.getAuth)
 
-  useEffect(() => {
-    getAuth();
-  }, []);
+    useEffect(() => {
+        getAuth()
+    }, [])
 
-  return (
-    <Stack.Navigator initialRouteName="Onboarding">
-      {authToken ? (
-        <>
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen
-            options={{ headerShown: false }}
-            name="Profile"
-            component={Profile}
-          />
-          <Stack.Screen name="Map" component={Map} />
-        </>
-      ) : (
-        <>
-          <Stack.Screen
-            options={{ headerShown: false }}
-            name="Onboarding"
-            component={Onboarding}
-          />
-          <Stack.Screen
-            options={{ headerShown: false }}
-            name="Login"
-            component={Login}
-          />
-          <Stack.Screen name="Register" component={Register} />
-        </>
-      )}
-    </Stack.Navigator>
-  );
+    return (
+        <Stack.Navigator initialRouteName='Onboarding'>
+            {!authToken ? (
+                <>
+                    <Stack.Screen name='Home' component={Home} />
+                    <Stack.Screen
+                        options={{ headerShown: false }}
+                        name='Profile'
+                        component={Profile}
+                    />
+                    <Stack.Screen name='Map' component={Map} />
+                </>
+            ) : (
+                <>
+                    <Stack.Screen
+                        options={{ headerShown: false }}
+                        name='Onboarding'
+                        component={Onboarding}
+                    />
+                    <Stack.Screen
+                        options={{ headerShown: false }}
+                        name='Login'
+                        component={Login}
+                    />
+                    <Stack.Screen name='Register' component={Register} />
+                </>
+            )}
+        </Stack.Navigator>
+    )
 }
--- front/src/views/Search.js
+++ front/src/views/Search.js
@@ -1,12 +1,120 @@
-import { View, Text } from 'react-native'
+import { useEffect } from 'react'
+import { Text, View, ScrollView, StyleSheet, Image } from 'react-native'
+import { useUsers } from '../features/search/hooks/useUsers'
+import { useNavigation } from '@react-navigation/native'
 
 const Search = () => {
+    const { users } = useUsers()
+    const navigation = useNavigation()
+
+    useEffect(() => {
+        navigation.setOptions({
+            headerLargeTitle: true,
+            headerTitle: 'Search',
+            headerRight: () => (
+                <TouchableOpacity
+                    onPress={() => navigation.navigate('Home')}
+                    style={{
+                        backgroundColor: 'purple',
+                        width: 30,
+                        height: 30,
+                        borderRadius: 10,
+                        justifyContent: 'center',
+                    }}
+                >
+                    <Text
+                        style={{
+                            fontSize: 20,
+                            textAlign: 'center',
+                            color: 'white',
+                        }}
+                    ></Text>
+                </TouchableOpacity>
+            ),
+            headerSearchBarOptions: {
+                placeholder: 'Search',
+                onChangeText: event => {
+                    searchFilterFunction(event.nativeEvent.text)
+                },
+            },
+        })
+    }, [navigation])
+
+    const searchFilterFunction = text => {
+        if (text) {
+            const newData = data.filter(item => {
+                const itemData = item.name.first
+                    ? item.name.first.toUpperCase()
+                    : ''.toUpperCase()
+                const textData = text.toUpperCase()
+
+                return itemData.indexOf(textData) > -1
+            })
+            setFilteredData(newData)
+        } else {
+            setFilteredData(data)
+        }
+    }
+
     return (
-        <View>
-            <Text>Search View</Text>
-            <Text>Buscar Jugadores:</Text>
-        </View>
+        <ScrollView>
+            <Text style={styles.textFriends}>Friends:</Text>
+            {users.map((user, index) => {
+                return (
+                    <View styles={styles.itemContainer} key={index}>
+                        <Image
+                            source={{
+                                uri: user.picture.large,
+                            }}
+                            style={styles.image}
+                        />
+                        <View>
+                            <Text style={styles.textName}>
+                                {user.name.first} {user.name.last}
+                            </Text>
+                            <Text stlye={styles.textEmail}>
+                                {user.login.username}
+                            </Text>
+                        </View>
+                    </View>
+                )
+            })}
+        </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    itemContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 10,
+        marginLeft: 10,
+    },
+    h2: {
+        fontSize: 20,
+    },
+    textFriends: {
+        fontSize: 20,
+        textAlign: 'left',
+        marginLeft: 10,
+        fontWeight: 'bold',
+        marginTop: 10,
+    },
+    image: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+    },
+    textName: {
+        fontSize: 17,
+        marginLeft: 10,
+        fontWeight: '600',
+    },
+    textEmail: {
+        fontSize: 14,
+        marginLeft: 10,
+        color: 'gray',
+    },
+})
 
 export default Search
