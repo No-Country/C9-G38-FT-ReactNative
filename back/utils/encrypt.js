f46968e3dd869abac8aa0c0c052ab24fecdf9629
Ren-9 <braianmartinez648@gmail.com>
2023-02-14 17:08:00 -0400

    fix: update

--- back/utils/encrypt.js
+++ back/utils/encrypt.js
@@ -2,8 +2,8 @@ const bcrypt = require('bcrypt');
 const SALT_ROUND = 10;
 
 const encrypt = async (password) => {
-  const newPass = await bcrypt.hash(password, SALT_ROUND);
-  return newPass;
+  const pass = await bcrypt.hash(password, SALT_ROUND);
+  return pass;
 };
 
 module.exports = { encrypt };
--- front/src/views/UpdateProfile.js
+++ front/src/views/UpdateProfile.js
@@ -1,51 +1,73 @@
-import { View, Text, TextInput, Button, StyleSheet, Image } from "react-native";
-import { useForm, Controller } from "react-hook-form";
-import Fonts from "../styles/theme/Fonts";
-import CSButton from "../common/ui/Button";
-
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, Image } from 'react-native';
+import { useForm, Controller } from 'react-hook-form';
+import Fonts from '../styles/theme/Fonts';
+import CSButton from '../common/ui/Button';
+import { useAuthStore } from '../store/authStore';
 
 const UpdateProfile = () => {
+  const authToken = useAuthStore((state) => state.authToken);
+  const [myProfile, setMyProfile] = useState();
+
   const {
     control,
     handleSubmit,
     formState: { errors },
   } = useForm({
     defaultValues: {
-      interest: "",
-      password: "",
-      phone: "",
+      interest: '',
+      password: '',
+      phone: '',
     },
   });
 
+  const getMyProfile = async () => {
+    let req = await fetch(
+      'https://c9-g38-ft-reactnative-production.up.railway.app/api/v1/auth/me',
+      {
+        method: 'GET',
+        headers: { Authorization: authToken },
+      }
+    );
+    let res = await req.json();
+    console.log(res.data);
+    setMyProfile(res.data);
+  };
+
+  useEffect(() => {
+    getMyProfile();
+  }, []);
+
   const onSubmit = () => {
-    console.log("hello world");
+    console.log('hello world');
   };
 
   return (
     <View style={styles.container}>
-      <Image
+      {/* <Image
         style={styles.profileImage}
         source={{
-          uri: "https://static.vecteezy.com/system/resources/thumbnails/002/534/006/small/social-media-chatting-online-blank-profile-picture-head-and-body-icon-people-standing-icon-grey-background-free-vector.jpg",
+          uri: 'https://static.vecteezy.com/system/resources/thumbnails/002/534/006/small/social-media-chatting-online-blank-profile-picture-head-and-body-icon-people-standing-icon-grey-background-free-vector.jpg',
         }}
       />
-      <Text style={styles.userName}>Username</Text>
-
+      <Text style={styles.userName}>Usernccame</Text> */}
+      <Text style={styles.subtitle}>Email:</Text>
       <Controller
         control={control}
         rules={{ required: true }}
+        name="email"
         render={({ field: { onChange, onBlur, value } }) => (
           <TextInput
             style={styles.input}
-            placeholder="Intereses"
+            placeholder="Email"
             onChangeText={onChange}
             onBlur={onBlur}
             value={value}
+            disableFullscreenUI={true}
           />
         )}
-        name="interest"
       />
+      <Text style={styles.subtitle}>Teléfono:</Text>
       <Controller
         control={control}
         rules={{ required: true }}
@@ -60,49 +82,36 @@ const UpdateProfile = () => {
         )}
         name="phone"
       />
+      <Text style={styles.subtitle}>Edad:</Text>
       <Controller
         control={control}
         rules={{ required: true }}
+        name="edad"
         render={({ field: { onChange, onBlur, value } }) => (
           <TextInput
             style={styles.input}
-            placeholder="Email"
+            placeholder="Edad"
             onChangeText={onChange}
             onBlur={onBlur}
             value={value}
+            disableFullscreenUI={true}
           />
         )}
-        name="email"
       />
+      <Text style={styles.subtitle}>Intereses:</Text>
       <Controller
         control={control}
         rules={{ required: true }}
+        name="interest"
         render={({ field: { onChange, onBlur, value } }) => (
           <TextInput
             style={styles.input}
-            placeholder="Contraseña"
-            secureTextEntry={true}
+            placeholder="Intereses"
             onChangeText={onChange}
             onBlur={onBlur}
             value={value}
           />
         )}
-        name="password"
-      />
-      <Controller
-        control={control}
-        rules={{ required: true }}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <TextInput
-            style={styles.input}
-            placeholder="Repetir contraseña"
-            secureTextEntry={true}
-            onChangeText={onChange}
-            onBlur={onBlur}
-            value={value}
-          />
-        )}
-        name="password"
       />
       <CSButton onPress={handleSubmit(onSubmit)} label="Guardar" />
     </View>
@@ -111,12 +120,13 @@ const UpdateProfile = () => {
 
 const styles = StyleSheet.create({
   container: {
-    height: "100%",
-    width: "100%",
-    alignItems: "center",
-    justifyContent: "space-between",
-    backgroundColor: "#fff",
+    height: '100%',
+    width: '100%',
+    alignItems: 'flex-start',
+    // justifyContent: 'space-between',
+    backgroundColor: '#fff',
     paddingHorizontal: 32,
+    paddingTop: 20,
   },
   profileImage: {
     height: 140,
@@ -124,21 +134,26 @@ const styles = StyleSheet.create({
     borderRadius: 140 / 2,
   },
   input: {
-    width: "100%",
+    width: '100%',
     height: 48,
-    backgroundColor: "#f8f8f8",
+    backgroundColor: '#f8f8f8',
     paddingLeft: 20,
     paddingVertical: 10,
-    marginBottom: 10,
+    marginBottom: 14,
     borderRadius: 12,
     fontSize: 15,
     fontFamily: Fonts.type.regular,
   },
   userName: {
-    textAlign: "center",
+    textAlign: 'center',
     fontSize: Fonts.size.xxxLarge,
     fontFamily: Fonts.type.bold,
     letterSpacing: 0.8,
+  },
+  subtitle: {
+    fontSize: Fonts.size.normal,
+    fontFamily: Fonts.type.semiBold,
+    color: 'gray',
   },
 });
 
