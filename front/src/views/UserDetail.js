c360f8392f68b2597282f11f348a511dbb5b3477
Ezequiel <caracido@gmail.com>
2023-02-20 10:23:35 -0300

    fix: avatar route

--- front/src/views/UserDetail.js
+++ front/src/views/UserDetail.js
@@ -71,7 +71,7 @@ const UserDetail = ({ route }) => {
           <Image
             style={styles.profileImage}
             source={{
-              uri: user.picture, // 'https://static.vecteezy.com/system/resources/thumbnails/002/534/006/small/social-media-chatting-online-blank-profile-picture-head-and-body-icon-people-standing-icon-grey-background-free-vector.jpg',
+              uri: user.avatar, // 'https://static.vecteezy.com/system/resources/thumbnails/002/534/006/small/social-media-chatting-online-blank-profile-picture-head-and-body-icon-people-standing-icon-grey-background-free-vector.jpg',
             }}
           />
         </View>
