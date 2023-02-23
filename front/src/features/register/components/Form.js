9b635368facd8376e96f82f401f1416ac61cc729
Nicolás <nicolaseugroberto@gmail.com>
2023-02-23 10:24:18 -0300

    FT: input titles

--- front/src/features/register/components/Form.js
+++ front/src/features/register/components/Form.js
@@ -1,13 +1,13 @@
-import { InputForm } from './InputForm';
-import { useForm } from 'react-hook-form';
-import { ScrollView, Text, View } from 'react-native';
-import { styles } from '../../../styles/register/register.style';
-import { inputs } from '../helpers/register-form';
-import { REGISTER_USER } from '../../../constants/endpoints';
-import { useNavigation } from '@react-navigation/native';
-import CSButton from '../../../common/ui/Button';
-import useFetch from '../../../hooks/useFetch';
-import URL from '../../../constants/endpoints';
+import { InputForm } from "./InputForm";
+import { useForm } from "react-hook-form";
+import { ScrollView, Text, View } from "react-native";
+import { styles } from "../../../styles/register/register.style";
+import { inputs } from "../helpers/register-form";
+import { REGISTER_USER } from "../../../constants/endpoints";
+import { useNavigation } from "@react-navigation/native";
+import CSButton from "../../../common/ui/Button";
+import useFetch from "../../../hooks/useFetch";
+import URL from "../../../constants/endpoints";
 
 const Form = () => {
   const {
@@ -23,14 +23,14 @@ const Form = () => {
     try {
       await connect({ url: URL.REGISTER, data: newUser });
 
-      navigation.navigate('Login');
+      navigation.navigate("Login");
     } catch (error) {
       console.log(error);
     }
   };
 
   return (
-    <ScrollView contentContainerStyle={styles.container}>
+    <ScrollView style={styles.container}>
       <View style={styles.headerWrapper}>
         {/* <Text style={styles.logo}>Logo Aplicacion o Imagen ilustrativa</Text> */}
         <Text style={styles.title}>Registrate!</Text>
@@ -44,6 +44,9 @@ const Form = () => {
       <View style={styles.formWrapper}>
         {inputs.map((input) => (
           <View key={input.name}>
+            <Text style={styles.inputTitle} key={input.title}>
+              {input.title}
+            </Text>
             <InputForm
               name={input.name}
               placeholder={input.placeholder}
@@ -53,7 +56,7 @@ const Form = () => {
               secure={input.secure}
               pwdValidate={input.pwdValidate}
             />
-            <Text style={{ color: 'red', margin: 3 }}>
+            <Text style={{ color: "red", margin: 3 }}>
               {errors[input.name]?.message}
             </Text>
           </View>
--- front/src/styles/register/register.style.js
+++ front/src/styles/register/register.style.js
@@ -1,34 +1,38 @@
-import { StyleSheet } from 'react-native';
-import Fonts from '../theme/Fonts';
+import { StyleSheet } from "react-native";
+import Fonts from "../theme/Fonts";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
+    alignItems: "center",
     margin: 10,
   },
   title: {
-    color: '#202020',
+    color: "#202020",
     fontSize: Fonts.size.xxxxLarge,
     fontFamily: Fonts.type.bold,
   },
   logo: {
-    justifyContent: 'center',
-    alignSelf: 'center',
+    justifyContent: "center",
+    alignSelf: "center",
   },
   logos: {
-    flexDirection: 'row',
+    flexDirection: "row",
     margin: 10,
   },
   h2: {
     fontSize: 14,
-    color: '#5b5463',
-    fontWeight: '500',
+    color: "#5b5463",
+    fontWeight: "500",
     marginBottom: 4,
+  },
+  inputTitle: {
+    fontSize: Fonts.size.normal,
+    fontFamily: Fonts.type.semiBold,
+    color: "gray",
   },
   input: {
     height: 48,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     paddingLeft: 20,
     paddingVertical: 10,
     borderRadius: 12,
