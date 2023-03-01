985493a1e591d5334984749be904a03cef74d058
Ezequiel <caracido@gmail.com>
2023-03-01 15:35:57 -0300

    feature: follow list view hardcode

--- front/src/views/RequestFollowers.js
+++ front/src/views/RequestFollowers.js
@@ -4,21 +4,25 @@ import CardItem from '../features/search/components/CardItem';
 import useFetch from '../hooks/useFetch';
 import { useAuthStore } from '../store/authStore';
 import { useUserStore } from '../store/userStore';
+import {data} from "../constants/data"
 
 
 const RequestFollowers = ({navigation}) => {
 
-    const { followerUsers, setFollowerUsers } = useUserStore((state) => state);
-    const [users, setUsers] = useState()
+  const { followerUsers, setFollowerUsers } = useUserStore((state) => state);
+    const [follower, setFollower] = useState(null)
+    const { authToken } = useAuthStore((state) => state);
+    
 
     const connect = useFetch();
 
     const geFollowtUsers = async () => {
-        
-    setFollowerUsers("hola")
-    console.log(followerUsers)
-        const resp = await connect({ url: URL.GET_FOLLOWERS});
-        setFollowerUsers(resp);
+      // console.log(followerUsers)
+      // const resp = await connect({ url: URL.GET_FOLLOWERS});
+      
+        setFollower(data)
+        setFollowerUsers(data)
+        // setFollowerUsers(resp);
         // setUsers(resp);
         
       };
@@ -26,26 +30,41 @@ const RequestFollowers = ({navigation}) => {
 
       useEffect(() => {
         geFollowtUsers()
-        
-      }, [])
+       
+       
+      }, [followerUsers])
       
-    
+      const acceptFollow = (index, id) =>{
+        const newArray = follower.splice(index, 1)
+        console.log(id)
+        setFollower(newArray)
+        
+
+      }
+      const declineFollow = (index, id) =>{
+        const newArray = follower.splice(index, 1)
+        setFollower(newArray)
+      }
 
   return (
   
       <View>
+        
         <FlatList
         data={followerUsers}
         keyExtractor={(item) => item.id}
-        renderItem={({ item: element }) => (
-     
+        renderItem={({ item: element, index}) => (
           <CardItem
             navigation={navigation}
-            avatar={element.avatar}
+            avatar={element.picture}
             username={element.username}
             intereses={element.sports}
             user={element}
             id={element.id}
+            acceptFollow = {acceptFollow}
+            declineFollow= {declineFollow}
+            index = {index}
+            buttons
           />
         )}
       />
