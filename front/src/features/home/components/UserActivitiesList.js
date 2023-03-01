cbf1564aedced902d72fc351ad7e9adcfae69748
Nicolás <nicolaseugroberto@gmail.com>
2023-03-01 15:40:26 -0300

    Ft: fetch suggested

--- front/src/features/home/components/UserActivitiesList.js
+++ front/src/features/home/components/UserActivitiesList.js
@@ -2,14 +2,35 @@ import { View, Text, StyleSheet, FlatList } from "react-native";
 import Fonts from "../../../styles/theme/Fonts";
 import { data } from "../../../constants/data";
 import UserFollowCard from "./UserFollowCard";
+//
+import useFetch from "../../../hooks/useFetch";
+import URL from "../../../constants/endpoints";
+import { useState, useEffect } from "react";
 
 const UserActivitiesList = () => {
+  //
+  const [suggested, setSuggested] = useState();
+  const connect = useFetch();
+  const MySuggested = async () => {
+    const resp = await connect({
+      url: URL.GET_FIRST_USERS,
+      extra: id,
+    });
+    console.log(resp);
+    setSuggested(resp);
+  };
+
+  useEffect(() => {
+    MySuggested();
+    console.log(suggested);
+  }, []);
+  //
   return (
     <View style={styles.wrapper}>
       <Text style={styles.subtitle}>Usuarios con tus mismos intereses:</Text>
       <FlatList
         numColumns={2}
-        data={data}
+        data={suggested}
         renderItem={({ item }) => <UserFollowCard key={item.id} item={item} />}
         keyExtractor={(item) => item.id}
       />
